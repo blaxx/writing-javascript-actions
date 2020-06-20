cdbc53bb8992dab27d31bb791192ec5ba15e7f90
Philipp Schwarz <philipp.schwarz@dasburo.com>
2020-06-20 21:10:10 +0200

    fix syntax error

--- .github/actions/hello-world/main.js
+++ .github/actions/hello-world/main.js
@@ -1,6 +1,6 @@
 const core = require("@actions/core");
 
-const firstGreeting = core.getInput("first-greeting"):
+const firstGreeting = core.getInput("first-greeting");
 const secondGreeting = core.getInput("second-greeting");
 const thirdGreeting = core.getInput("third-greeting");
 
