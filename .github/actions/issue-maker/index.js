b523ef6348a6c5a188a064af2e60b53b11d8e551
Philipp Schwarz <philipp.schwarz@dasburo.com>
2020-06-20 22:07:28 +0200

    fix token variable

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -7,7 +7,7 @@ async function run() {
     const jokeBody = core.getInput("joke");
     const token = core.getInput("repo-token");
 
-    const octokit = github.getOctokit(myToken);
+    const octokit = github.getOctokit(token);
 
     const newIssue = await octokit.issues.create({
       repo: github.context.repo.repo,
